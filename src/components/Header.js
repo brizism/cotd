8802f7b4501c3f7cc04d024ac09dcdf386c5eceb
brizism <montielbriseida@gmail.com>
2018-03-06 12:19:59 -0500

    update header to stateles func comp

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,18 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Header extends Component {
-  render(){
-    return (
-      <header className="top">
-        <h1>
-        Catch
-        <span className="ofThe">
-          <span className="of">Of</span>
-          <span className="the">The</span>
-        </span>
-        </h1>
-        <h3 className="tagline">
-          <span>Fresh SeaFood Market</span>
-        </h3>
-      </header>
-    )
-  }
-}
+const Header = props => (
+  <header className="top">
+    <h1>
+    Catch
+    <span className="ofThe">
+      <span className="of">Of</span>
+      <span className="the">The</span>
+    </span>
+    </h1>
+    <h3 className="tagline">
+      <span>{props.tagline}</span>
+    </h3>
+  </header>
+)
 
 export default Header;
