d6bd8982d94f710a29a5f66e9e980abe55047be2
brizism <montielbriseida@gmail.com>
2018-03-06 14:12:05 -0500

    add property that changes pages using history.push

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,13 +2,26 @@ import React, { Component, Fragment } from 'react';
 import { getFunName } from '../helpers';
 
 class StorePicker extends Component {
+  myInput = React.createRef();
+
+  goToStore = e => {
+    e.preventDefault();
+    const storeName = this.myInput.value.value;
+    this.props.history.push(`/store/${storeName}`)
+  }
   render(){
     return (
       <Fragment>
         <p>Fish!</p>
-        <form className="store-selector">
+        <form className="store-selector" onSubmit={this.goToStore}>
           <h2>Please Enter a store</h2>
-          <input type="text" required placeholder="Store Name" defaultValue={getFunName()}/>
+          <input 
+            type="text" 
+            ref={this.myInput}
+            required 
+            placeholder="Store Name" 
+            defaultValue={getFunName()}
+          />
           <button type="submit">Visit Store</button>
         </form> 
       </Fragment>
