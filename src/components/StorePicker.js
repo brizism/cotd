fb824bebd80312cf3973120bf65aa4c92032adf1
brizism <montielbriseida@gmail.com>
2018-03-06 11:34:19 -0500

    add StorePicker component

---
+++ src/components/StorePicker.js
@@ -0,0 +1,19 @@
+import React, { Component, Fragment } from 'react';
+
+class StorePicker extends Component {
+  render(){
+    return (
+      <Fragment>
+        <p>Fish!</p>
+        <form className="store-selector">
+          <h2>Please Enter a store</h2>
+          <input type="text" required placeholder="Store Name"/>
+          <button type="submit">Visit Store</button>
+        </form> 
+      </Fragment>
+         
+    ) 
+  }
+}
+
+export default StorePicker; 
