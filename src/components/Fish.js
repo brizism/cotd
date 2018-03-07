4b8181a315ef953e16c8ca2b3766e3c4f5d71acc
brizism <montielbriseida@gmail.com>
2018-03-06 20:31:37 -0500

    add condition to button based on availability

--- src/components/Fish.js
+++ src/components/Fish.js
@@ -3,7 +3,8 @@ import { formatPrice } from '../helpers';
 
 class Fish extends Component {
   render(){
-    const { image, name, price, desc, status } = this.props.details;
+    const { image, name, price, desc, status } = this.props.details; //destructure details obj passed down as props
+    const isAvailable = status === 'available';
     return (
       <li className="menu-fish">
         <img src={image} alt={name}/>
@@ -12,7 +13,7 @@ class Fish extends Component {
           <span className="price">{formatPrice(price)}</span>
         </h3>
         <p>{desc}</p>
-        <button>Add To Cart</button>
+        <button disabled={!isAvailable}>{isAvailable ? 'Add To Order' : 'Sold out!'}</button>
       </li>
     )
   }
