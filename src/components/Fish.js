f7d881bca42abcb7d6a287615456468a948479b6
brizism <montielbriseida@gmail.com>
2018-03-06 21:23:28 -0500

    attach add method to button

--- src/components/Fish.js
+++ src/components/Fish.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import { formatPrice } from '../helpers';
 
 class Fish extends Component {
+  handleClick = () => {
+    this.props.addToOrder(this.props.index);
+  }
   render(){
     const { image, name, price, desc, status } = this.props.details; //destructure details obj passed down as props
     const isAvailable = status === 'available';
@@ -13,7 +16,9 @@ class Fish extends Component {
           <span className="price">{formatPrice(price)}</span>
         </h3>
         <p>{desc}</p>
-        <button disabled={!isAvailable}>{isAvailable ? 'Add To Order' : 'Sold out!'}</button>
+        <button disabled={!isAvailable} onClick={this.handleClick}>
+          {isAvailable ? 'Add To Order' : 'Sold out!'}
+        </button>
       </li>
     )
   }
