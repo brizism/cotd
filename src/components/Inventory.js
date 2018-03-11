2eb6b78236d7fb4bf83507fc0ef05c4ed3cb0e87
brizism <montielbriseida@gmail.com>
2018-03-11 16:37:03 -0400

    instatiate firebase.auth

--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import AddFishForm from './AddFishForm';
 import EditFishForm from './EditFishForm';
+import Login from './Login';
+import firebase from 'firebase';
+import { firebaseApp } from '../base';
 
 class Inventory extends Component {
   static propTypes = {
@@ -10,7 +13,21 @@ class Inventory extends Component {
     deleteFish: PropTypes.func,
     loadSampleFishes: PropTypes.func
   }
+
+  authHandler = async (authData) => {
+    console.log(authData);
+  }
+
+  authenticate = provider => {
+    const authProvider = new firebase.auth[`${provider}AuthProvider`]();
+    firebaseApp
+      .auth()
+      .signInWithPopup(authProvider)
+      .then(this.authHandler);
+  };
+
   render(){
+    return <Login authenticate={this.authenticate}/>
     return (
       <div className="inventory">
         <h2>Inventory</h2>
